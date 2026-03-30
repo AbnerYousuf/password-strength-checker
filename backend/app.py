3cab8ba2153140afaac8211ed816cd489dd92a1f
AbnerYousuf <yousufabner@gmail.com>
2026-03-30 13:04:08 -0600

    setting up CORS to communicate wiht frontend

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel #simplifies handling requests
+import zxcvbn #api for checking the pw strength
+
+app = FastAPI()
+
+# this block is for communicating with the frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # this is the vite dev server address
+    #so all requests are allowed from it
+    allow_credentials=True,
+    allow_methods=["*"], #allow all methods (get/post)
+    allow_headers=["*"], #allow all headers
+)
