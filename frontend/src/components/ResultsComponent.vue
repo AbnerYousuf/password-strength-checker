144145dc0a3f830aa62dd78b431dde000f7d7d58
AbnerYousuf <yousufabner@gmail.com>
2026-03-30 21:56:22 -0600

    added resultscomponent and resultslist type for password audit results

---
+++ frontend/src/components/ResultsComponent.vue
@@ -0,0 +1,17 @@
+<template>
+{{props.password}}
+{{props.results.score}}
+{{ props.results.guesses }}
+{{props.results.feedback.suggestions}}
+{{props.results.feedback.warning}}
+{{ props.results.crack_times_display}} }}
+</template>
+
+<script setup lang="ts">
+import type { ResultsList } from '@/stores/types'
+
+const props = defineProps<{
+  password: string
+  results: ResultsList
+}>()
+</script>
---
+++ frontend/src/stores/types.ts
@@ -0,0 +1,11 @@
+export interface ResultsList {
+  score: number;
+  feedback: {
+    warning: string;
+    suggestions: string[];
+  };
+  guesses: number;
+  crack_times_display: {
+    [key: string]: string;
+  };
+}
