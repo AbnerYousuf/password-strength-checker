5153d5050750ae1ff11c456176b6fb52decb836a
AbnerYousuf <yousufabner@gmail.com>
2026-03-31 15:48:35 -0600

    added initial display of password audit results to frontend (please dont use it like this)

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,10 +4,25 @@
   >
     <div><PageHeaderComponent /></div>
     <div><FormComponent /></div>
+    <div v-if="lastEntry" class="mt-8">
+      <ResultsComponent
+        :password="lastEntry.password"
+        :results="lastEntry.results"
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
 import PageHeaderComponent from '@/components/PageHeaderComponent.vue'
 import FormComponent from '@/components/FormComponent.vue'
+import ResultsComponent from '@/components/ResultsComponent.vue'
+import { usePasswordStore } from '@/stores/passwordStore'
+
+const store = usePasswordStore()
+const lastEntry = computed(() => {
+  const history = store.passwordHistory
+  return history.length > 0 ? history[history.length - 1] : null
+})
 </script>
