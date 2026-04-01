600b47087c29550f921a46b08b1ef588ec37e637
AbnerYousuf <yousufabner@gmail.com>
2026-04-01 12:43:57 -0600

    fixed up notfoundview

--- frontend/src/views/NotFoundView.vue
+++ frontend/src/views/NotFoundView.vue
@@ -2,7 +2,7 @@
     <div class="not-found">
         <h1>404 - Page Not Found</h1>
         <p>The page you're looking for doesn't exist.</p>
-        <router-link to="/">Go Home</router-link>
+        <router-link to="/" class="text-blue-600 underline hover:text-blue-900">Go Home</router-link>
     </div>
 </template>
 
