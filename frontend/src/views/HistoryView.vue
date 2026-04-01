226666668397a69453f67506d1d158dd3ca89cfd
AbnerYousuf <yousufabner@gmail.com>
2026-04-01 14:31:16 -0600

    formatting

--- frontend/src/views/HistoryView.vue
+++ frontend/src/views/HistoryView.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="p-4 max-w-4xl mx-auto">
     <h2 class="text-2xl font-semibold mb-6">Password Audit History</h2>
-    
+
     <div v-if="passwordHistory.length === 0">
-      No passwords audited yet. Try out the <RouterLink :to="{ name: 'home' }">Home</RouterLink> page!
+      No passwords audited yet. Try out the
+      <RouterLink :to="{ name: 'home' }">Home</RouterLink> page!
     </div>
-    
+
     <div v-else class="flex flex-col gap-4">
-      <div 
-        v-for="(entry, index) in [...passwordHistory].reverse()" 
+      <div
+        v-for="(entry, index) in [...passwordHistory].reverse()"
         :key="index"
         class="bg-white rounded-lg shadow p-4"
       >
